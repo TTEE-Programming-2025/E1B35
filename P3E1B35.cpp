8e600271f428be839a3ee04406324e45279d1fa0
E1B35 郭宥宏 <u11302235@o365.ttu.edu.tw>
2025-05-09 21:50:28 +0800

    forth commit:做了b部分的1人部分，並且先打好2~4人的else if，然後判斷是否滿意，如果滿意就把'@'換成'*'，如果不滿意就把'@'換回'-'。

--- P3E1B35.cpp
+++ P3E1B35.cpp
@@ -42,6 +42,80 @@ int main(){
 	    }
             printf("\n按任意鍵返回主選單...\n");
             getch();
-        } 
     } 
+    
+    else if (choice=='b'||choice=='B') { // 當輸入 b 或 B 時 
+            int n=0;
+            printf("請輸入需要幾個座位（1~4）：");
+            scanf("%d", &n);
+            
+            while(n<1||n>4){ // 重複輸入直到是 1~4 
+            	printf("錯誤!\n\n請輸入 1~4 的整數：");
+                scanf(" %d",&n);
+            }
+            printf("\n");
+
+            if (n==1) {
+            	count=0;
+                while(count!=1){
+                    int x = rand() % 9;
+                    int y = rand() % 9;
+                    if(seat[x][y] != '*'){
+                        seat[x][y] = '@';
+                        
+                        count++;
+                    }
+                } 
+            }
+            if (n==2) {
+                printf("暫時\n");
+            }
+            if (n==3) {
+                printf("暫時\n");
+            }
+            if (n==4) {
+                printf("暫時\n");
+            }
+            printf("\\ 1 2 3 4 5 6 7 8 9\n");
+	       	for(int i=0;i<9;i++){ //印座位表  
+		        printf("%d ",9-i);
+			    for(int j=0;j<9;j++){
+			        printf("%c ",seat[i][j]);
+		        }
+		    printf("\n");
+	        } 
+	        
+	        char yn;
+	        printf("你滿意嗎?(y/n):");
+	        scanf(" %c",&yn);
+            
+            while(yn!='y'&&yn!='n'){ // 重複輸入直到是 y 或 n 
+            	printf("錯誤!\n請輸入 y 或 n\n\n你滿意嗎?(y/n):");
+            	fflush(stdin);
+                scanf(" %c",&yn);
+            }
+			 
+            if (yn=='y'||yn=='Y') { // 當輸入 n 或 N 時 
+                for(int i=0;i<9;i++){ //將 @ 換成 *   
+		            for(int j=0;j<9;j++){
+			            if(seat[i][j]=='@'){
+				            seat[i][j] = '*';
+			            }
+		            }
+	            }
+	        }
+	        if (yn=='n'||yn=='N') { // 當輸入 n 或 N 時 
+                for(int i=0;i<9;i++){ //將 @ 換成 -    
+		            for(int j=0;j<9;j++){
+			            if(seat[i][j]=='@'){
+				            seat[i][j] = '-';
+			            }
+		            }
+	            }
+	        }
+            printf("\n按任意鍵返回主選單...\n");
+            getch();
+        }
+    } 
+    
 }
