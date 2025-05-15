5e93f227c5bb9bda70945fee1716763c0360f220
E1B35 郭宥宏 <u11302235@o365.ttu.edu.tw>
2025-05-15 20:51:06 +0800

    second commit:上次沒發現⌈學生資料請用結構的陣列實現⌋這句話，所以把之前的進度都改成符合要求的樣子。

--- P4E1B35.cpp
+++ P4E1B35.cpp
@@ -14,15 +14,21 @@ void acase(void){
 	        getch();
 	        continue;
 	    }
-	    char name[n][100]={'0'};
-	    int num[n]={0};
+	    struct data{
+	    	char name[100];
+	        int num;
+	        int math;
+	        int physics;
+	        int english;
+		}student[n];
+	    
 	    for(int i=0;i<n;i++){
 	    	printf("學生姓名:");
-	    	scanf("%s",&name[i]);
+	    	scanf("%s",&student[i].name);
 	    	
 	    	printf("學號（6 位整數）:");
-	    	scanf("%d",&num[i]);
-	    	if (num[i]<0||num[i]>999999){
+	    	scanf("%d",&student[i].num);
+	    	if (student[i].num<0||student[i].num>999999){
 	    		printf("錯誤! 請重新輸入!\n\n");
 	            printf("按下任意鍵重新輸入 . . . ");
 	            getch();
