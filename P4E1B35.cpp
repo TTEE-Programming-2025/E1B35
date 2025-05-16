0b2383f3da5e2fdd62ea09f886067be314c84131
E1B35 郭宥宏 <u11302235@o365.ttu.edu.tw>
2025-05-17 01:44:15 +0800

    third commit:發現⌈立刻出現錯誤訊息並要求改正⌋這句話，應該是⌈立刻出現錯誤訊息，同時要求改正⌋，而不是我之前理解的⌈立刻出現錯誤訊息，並要求從頭到尾全部改正⌋，所以我改寫我把要輸入資訊時的程式都改成單獨的無窮迴圈，直到正確才跳出。另外，⌈學號（6 位整數）⌋這個要求中，000000應該不能算作是六位數的整數，所以我把判斷範圍改成100000~999999。

--- P4E1B35.cpp
+++ P4E1B35.cpp
@@ -2,10 +2,10 @@
 #include<stdlib.h>
 #include<conio.h>
 void acase(void){
-	int check=0;
-	while(check==0){
+	int n=0;
+	while(1){ /*輸入 n 值，以及判斷是否為 5~10*/ 
 		system("cls");
-	    int n=0;
+	    n=0;
 	    printf("請輸入學生個數(5~10):");
 	    scanf(" %d",&n);
 	    if (n>10||n<5){
@@ -14,26 +14,31 @@ void acase(void){
 	        getch();
 	        continue;
 	    }
-	    struct data{
-	    	char name[100];
-	        int num;
-	        int math;
-	        int physics;
-	        int english;
-		}student[n];
+	    break;
+	}
+	struct data{
+	    char name[100];
+	    int num;
+	    int math;
+	    int physics;
+	    int english;
+	}student[n];
 	    
-	    for(int i=0;i<n;i++){
-	    	printf("學生姓名:");
-	    	scanf("%s",&student[i].name);
-	    	
-	    	printf("學號（6 位整數）:");
+	for(int i=0;i<n;i++){ /*輸入 n 筆資料*/ 
+	    
+		printf("學生姓名:");
+	    scanf("%s",&student[i].name); /*輸入學生姓名*/
+	    
+		while (1){ /*輸入學號，以及判斷是否為 100000~999999*/ 
+			printf("學號（6 位整數）:");
 	    	scanf("%d",&student[i].num);
-	    	if (student[i].num<0||student[i].num>999999){
+	    	if (student[i].num<100000||student[i].num>999999){
 	    		printf("錯誤! 請重新輸入!\n\n");
 	            printf("按下任意鍵重新輸入 . . . ");
 	            getch();
-	            break;
+	            continue;
 			}
+			break;
 		}
 	}
 	
