3e4e8421cbdcad380e9d6c76c1685539c581da1c
E1B35 郭宥宏 <u11302235@o365.ttu.edu.tw>
2025-05-17 17:18:33 +0800

    fourth commit:增加輸入數學、物理、英文的程式碼，完成第三題的要求。

--- P4E1B35.cpp
+++ P4E1B35.cpp
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #include<conio.h>
 void acase(void){
-	int n=0;
+	int n;
 	while(1){ /*輸入 n 值，以及判斷是否為 5~10*/ 
 		system("cls");
 	    n=0;
@@ -23,14 +23,15 @@ void acase(void){
 	    int physics;
 	    int english;
 	}student[n];
-	    
+	
 	for(int i=0;i<n;i++){ /*輸入 n 筆資料*/ 
-	    
-		printf("學生姓名:");
-	    scanf("%s",&student[i].name); /*輸入學生姓名*/
+	    system("cls");
+		printf("第%d位學生姓名:",i+1);
+	    scanf("%s",&*student[i].name); /*輸入學生姓名*/
 	    
 		while (1){ /*輸入學號，以及判斷是否為 100000~999999*/ 
-			printf("學號（6 位整數）:");
+			system("cls");
+			printf("%s的學號（6 位整數）:",student[i].name);
 	    	scanf("%d",&student[i].num);
 	    	if (student[i].num<100000||student[i].num>999999){
 	    		printf("錯誤! 請重新輸入!\n\n");
@@ -40,14 +41,58 @@ void acase(void){
 			}
 			break;
 		}
+		
+		while (1){ /*輸入數學成績，以及判斷是否為 0~100*/ 
+			system("cls");
+			printf("%s的數學成績（0 ~ 100）:",student[i].name);
+	    	scanf("%d",&student[i].math);
+	    	if (student[i].math<0||student[i].math>100){
+	    		printf("錯誤! 請重新輸入!\n\n");
+	            printf("按下任意鍵重新輸入 . . . ");
+	            getch();
+	            continue;
+			}
+			break;
+		}
+		
+		while (1){ /*輸入物理成績，以及判斷是否為 0~100*/ 
+			system("cls");
+			printf("%s的物理成績（0 ~ 100）:",student[i].name);
+	    	scanf("%d",&student[i].physics);
+	    	if (student[i].physics<0||student[i].physics>100){
+	    		printf("錯誤! 請重新輸入!\n\n");
+	            printf("按下任意鍵重新輸入 . . . ");
+	            getch();
+	            continue;
+			}
+			break;
+		}
+		
+		while (1){ /*輸入英文成績，以及判斷是否為 0~100*/ 
+			system("cls");
+			printf("%s的英文成績（0 ~ 100）:",student[i].name);
+	    	scanf("%d",&student[i].english);
+	    	if (student[i].english<0||student[i].english>100){
+	    		printf("錯誤! 請重新輸入!\n\n");
+	            printf("按下任意鍵重新輸入 . . . ");
+	            getch();
+	            continue;
+			}
+			break;
+		}
 	}
 	
+	printf("\n按下任意鍵回到主選單 . . . ");
+	getch();
 }
 int main(){
-	char chiose;
-	printf("請輸入效果");
-	scanf("%c",&chiose);
-	if(chiose=='a'){
-		acase();
+	while (1){
+		char chiose;
+	    printf("請輸入效果");
+	    scanf("%c",&chiose);
+	    if(chiose=='a'){
+		    acase();
+	    }
 	}
+	
 }
