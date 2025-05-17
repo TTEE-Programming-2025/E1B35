4678c865dd85aac31d971d04fb109a7f10d82c8f
E1B35 郭宥宏 <u11302235@o365.ttu.edu.tw>
2025-05-17 18:27:18 +0800

    fifth commit:當我開始做第四題時，發現之前的寫法無法讓除了a的其他地方無法讀到陣列和n的值，所以把他們變成全域變數，讓b能夠讀到他們的資料。

--- P4E1B35.cpp
+++ P4E1B35.cpp
@@ -1,14 +1,23 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<conio.h>
+
+struct data{
+	    char name[100];
+	    int num;
+	    int math;
+	    int physics;
+	    int english;
+	}student[10];
+int n;
+
 void acase(void){
-	int n;
 	while(1){ /*輸入 n 值，以及判斷是否為 5~10*/ 
 		system("cls");
 	    n=0;
 	    printf("請輸入學生個數(5~10):");
 	    scanf(" %d",&n);
-	    if (n>10||n<5){
+	    if (n>10||n<1){ /* !!!!! 記得改回 5~10 !!!!! */ 
 	        printf("錯誤! 請重新輸入!\n\n");
 	        printf("按下任意鍵重新輸入 . . . ");
 	        getch();
@@ -16,23 +25,16 @@ void acase(void){
 	    }
 	    break;
 	}
-	struct data{
-	    char name[100];
-	    int num;
-	    int math;
-	    int physics;
-	    int english;
-	}student[n];
 	
 	for(int i=0;i<n;i++){ /*輸入 n 筆資料*/ 
 	    system("cls");
 		printf("第%d位學生姓名:",i+1);
-	    scanf("%s",&*student[i].name); /*輸入學生姓名*/
+	    scanf(" %s",&student[i].name); /*輸入學生姓名*/
 	    
 		while (1){ /*輸入學號，以及判斷是否為 100000~999999*/ 
 			system("cls");
 			printf("%s的學號（6 位整數）:",student[i].name);
-	    	scanf("%d",&student[i].num);
+	    	scanf(" %d",&student[i].num);
 	    	if (student[i].num<100000||student[i].num>999999){
 	    		printf("錯誤! 請重新輸入!\n\n");
 	            printf("按下任意鍵重新輸入 . . . ");
@@ -45,7 +47,7 @@ void acase(void){
 		while (1){ /*輸入數學成績，以及判斷是否為 0~100*/ 
 			system("cls");
 			printf("%s的數學成績（0 ~ 100）:",student[i].name);
-	    	scanf("%d",&student[i].math);
+	    	scanf(" %d",&student[i].math);
 	    	if (student[i].math<0||student[i].math>100){
 	    		printf("錯誤! 請重新輸入!\n\n");
 	            printf("按下任意鍵重新輸入 . . . ");
@@ -58,7 +60,7 @@ void acase(void){
 		while (1){ /*輸入物理成績，以及判斷是否為 0~100*/ 
 			system("cls");
 			printf("%s的物理成績（0 ~ 100）:",student[i].name);
-	    	scanf("%d",&student[i].physics);
+	    	scanf(" %d",&student[i].physics);
 	    	if (student[i].physics<0||student[i].physics>100){
 	    		printf("錯誤! 請重新輸入!\n\n");
 	            printf("按下任意鍵重新輸入 . . . ");
@@ -71,7 +73,7 @@ void acase(void){
 		while (1){ /*輸入英文成績，以及判斷是否為 0~100*/ 
 			system("cls");
 			printf("%s的英文成績（0 ~ 100）:",student[i].name);
-	    	scanf("%d",&student[i].english);
+	    	scanf(" %d",&student[i].english);
 	    	if (student[i].english<0||student[i].english>100){
 	    		printf("錯誤! 請重新輸入!\n\n");
 	            printf("按下任意鍵重新輸入 . . . ");
@@ -81,17 +83,25 @@ void acase(void){
 			break;
 		}
 	}
-	
 	printf("\n按下任意鍵回到主選單 . . . ");
 	getch();
 }
-int main(){
+void bcase(void){
+	for(int i=0;i<n;i++){ /*輸入 n 筆資料*/ 
+		printf("%s\n",student[i].name);
+	}
+}
+int main(void){
 	while (1){
 		char chiose;
-	    printf("請輸入效果");
-	    scanf("%c",&chiose);
+	    printf("請輸入效果:");
+	    scanf(" %c",&chiose);
 	    if(chiose=='a'){
 		    acase();
+		    system("cls");
+	    }
+	    else if(chiose=='b'){
+		    bcase();
 	    }
 	}
 	
